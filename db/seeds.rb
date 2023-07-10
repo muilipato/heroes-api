864426ad39497c186209d7ffa276f9c6a1a37f8a
Patrick <muilipato@gmail.com>
2023-07-10 12:03:45 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Create some heroes
+Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+Hero.create(name: "Bruce Wayne", super_name: "Batman")
+Hero.create(name: "Clark Kent", super_name: "Superman")
+Hero.create(name: "Tony Stark", super_name: "Iron Man")
+
+# Create some powers
+Power.create(name: "super strength", description: "gives the wielder super-human strengths")
+Power.create(name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed")
+Power.create(name: "heat vision", description: "emits beams of intense heat from the eyes")
+Power.create(name: "genius intellect", description: "possesses exceptional intelligence and inventiveness")
+
+# Create associations between heroes and powers
+HeroPower.create(hero: Hero.first, power: Power.first, strength: "Strong")
+HeroPower.create(hero: Hero.first, power: Power.second, strength: "Average")
+HeroPower.create(hero: Hero.fourth, power: Power.first, strength: "Strong")
+HeroPower.create(hero: Hero.fifth, power: Power.fourth, strength: "Average")
